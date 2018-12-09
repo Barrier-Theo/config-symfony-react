f033e5496ee2220289785db50afd13c494a89f8b
Théo Barrier <theo.barrier@hotmail.fr>
2018-12-09 17:30:48 +0100

    INITIALISATION DU PROJET

---
+++ app/webapp/components/aTestComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './aTestComponent.css';
+
+const MyComponent = () => <h1 className="montitre">Hello World LP DIWA!</h1>
+
+export default MyComponent
---
+++ app/webapp/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyComponent from './components/aTestComponent';
+
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('root')
+);
